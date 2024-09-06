1741fc249853c8e757818e1c815774ccceeb91af
aashish0959 <162534187+aashish0959@users.noreply.github.com>
2024-09-06 13:23:29 +0530

    event

--- First-day/src/Componet/Eventes.jsx
+++ First-day/src/Componet/Eventes.jsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import { useState } from 'react'
+import React , { useState } from 'react'
+
 
 export default function Eventes() {
     const[count,setCount]= useState(0)
@@ -7,10 +7,11 @@ export default function Eventes() {
     const[sub,setSub]= useState("")
     
     const hendleSubmit = (event) =>{
+        event.preventDefault()
         console.log ("form submitted")
         let obj = {
-            name : name ,
-            sunject : sub
+            name: name ,
+            sunject: sub
         }
         console.log(obj)
     }
@@ -21,8 +22,8 @@ export default function Eventes() {
         <button onClick={() => setCount(count+1)}>Clik</button>
         <br />
         <form onSubmit={(event)=> hendleSubmit(event) }>
-        <input type="text" placeholder='Enter the name' onClick={(e) => setName(e.target.value)} /> <br />
-        <input type="text" placeholder='Enter the sub' onClick={(e) => setSub(e.target.value)} /> <br />
+        <input type="text" placeholder='Enter the name' onChange={(e) => setName(e.target.value)} /> <br />
+        <input type="text" placeholder='Enter the sub' onChange={(e) => setSub(e.target.value)} /> <br />
         <button type='submit'>Submit</button>
 
         </form>
