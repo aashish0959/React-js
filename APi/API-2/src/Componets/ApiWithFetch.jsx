cd63e39d81f1223d51d2f365596d082520019941
aashish0959 <162534187+aashish0959@users.noreply.github.com>
2024-10-22 00:25:08 +0530

    Api-2

--- APi/API-2/src/Componets/ApiWithFetch.jsx
+++ APi/API-2/src/Componets/ApiWithFetch.jsx
@@ -1,20 +1,55 @@
 import axios from "axios";
-import React, { useState } from "react";
-import { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 
 export default function ApiWithFetch() {
-  const [record, setRecord] = useState();
+  const [record, setRecord] = useState([]);
+
   useEffect(() => {
     fetchApi();
   }, []);
-  const fetchApi = async () => {
-    let response = await axios.get("http://localhost:5000/posts");
-    console.log(response.data);
-    setRecord(response.data);
-  };
-  return 
-  <div>
-    <h1>ApiWithFetch</h1>
 
-    </div>;
+  const fetchApi = async () => {
+    
+      let response = await axios.get("http://localhost:5000/posts");
+      console.log(response.data);
+      setRecord(response.data);
+
+  };
+
+  const addData = async () => {
+
+    let response = await axios.post("http://localhost:5000/posts", {
+      id: "5",
+      title: "another title",
+      views: "asfd"
+    });
+    console.log(response.data);
+    setRecord([...record, response.data]);
+   
+  };
+
+
+
+  return (
+    <div>
+      <h1>ApiWithFetch</h1>
+      <button onClick={addData}>Add Data</button>
+      {record &&
+        record.map((e, i) => {
+          return (
+            <ul key={i}>
+              <li>{e.id}</li>
+              <li>{e.title}</li>
+              <li>{e.views}</li>
+              <li>
+                <button onClick={() => deleteData(e.id)}>Delete</button>
+              </li>
+              <li>
+                <button>Edit</button>
+              </li>
+            </ul>
+          );
+        })}
+    </div>
+  );
 }
